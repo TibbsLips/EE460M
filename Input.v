ca0a5225a86637c8c8ad3a711457967f1983591d
TibbsLips <32143495+TibbsLips@users.noreply.github.com>
2017-10-21 18:25:29 -0500

    Update Input.v

--- Input.v
+++ Input.v
@@ -24,14 +24,15 @@ module debounce(clk,in,out);
   wire q2n; //qn output from flip flop 2
 
   DFF firstFlop(in,clk,q1,q1n);
-  DFF secondFlop(out1,clk,q2,q2n);
+  DFF secondFlop(q1,clk,q2,q2n);
 
-  assign out= out2;
+  assign out= q2;
 
 endmodule
 
 
-module input(clk,btnU,btnL,btnR,btnD,sw0,sw1,mode,time);
+module inputs(clk,btnU,btnL,btnR,btnD,sw0,sw1,mode,times);
+  input clk;
   input btnU;            //add 50
   input btnL;            //add 150
   input btnR;            //add 200
@@ -40,7 +41,7 @@ module input(clk,btnU,btnL,btnR,btnD,sw0,sw1,mode,time);
   input sw1;             //reset to 205s
 
   output reg mode;     //will send final LED status from
-  output reg [13:0]time;  //will have the time to display on led
+  output reg [13:0]times;  //will have the time to display on led
 
 
   wire btnUstable;        //make sure to debounce
@@ -63,8 +64,7 @@ module input(clk,btnU,btnL,btnR,btnD,sw0,sw1,mode,time);
 
   initial
     begin
-      state=0; //state0 means no time, flashing
-      time=0;
+      times=0;
       mode=0;
     end
 
@@ -73,100 +73,100 @@ always@(posedge clk)
 
   if(btnUstable==1)
     begin
-      if(time+50<=9999)
+      if(times+50<=9999)
         begin
-          time=time+50;
+          times=times+50;
         end
       else
         begin
-          time=9999;
+          times=9999;
         end
     end
 
   else if(btnLstable==1)
     begin
-      if(time+150<=9999)
+      if(times+150<=9999)
         begin
-          time=time+150;
+          times=times+150;
         end
       else
         begin
-          time=9999;
+          times=9999;
         end
     end
 
   else if(btnRstable==1)
     begin
-      if(time+200<=9999)
+      if(times+200<=9999)
         begin
-         time=time+200;
+         times=times+200;
         end
       else
         begin
-          time=9999;
+          times=9999;
         end
     end
 
   else if(btnDstable==1)
     begin
-      if(time+500<=9999)
+      if(times+500<=9999)
         begin
-          time=time+500;
+          times=times+500;
         end
       else
         begin
-          time=9999;
+          times=9999;
         end
     end
 
   else if(sw0==1)
     begin
-      time=10;
+      times=10;
     end
 
-  else if(sw1=1)
+  else if(sw1==1)
     begin
-      time=205;
+      times=205;
     end
 
   else
       begin
-        time=time;
+        times=times;
       end
   //////////////////////////////////////////////
   ////we need to send numbers with BCD to 7-seg
   //////////////////////////////////////////////
   /////end of time update, time to decrement and flash
 
-  if(time==0)
+  if(times==0)
     begin
       mode=2; //blink at 0.5 Hz
-      time=0;
+      times=0;
     end
 
-  else if((time<=200)&&(time>0))
+  else if((times<=200)&&(times>0))
     begin
       mode=3; //blink at 1Hz
-      if(time%2==1)
+      if(times%2==1)
         begin
-          time=time-1;
+          times=times-1;
         end
       else
         begin
-          time=time-2; //This will let the display skip odd numbers
+          times=times-2; //This will let the display skip odd numbers
         end
     end
 
-  else if(time>200)
+  else if(times>200)
     begin
       mode=1; //LED's on
-      time=time-1;
+      times=times-1;
     end
 
   else
     begin
         mode=0;
-        time=time;
+        times=times;
     end
 end
 endmodule
