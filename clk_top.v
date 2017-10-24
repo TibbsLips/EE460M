ec6c75aeb29f2fbf11ef42e4acec46cd839a1339
HenrySATX <henry.song@utexas.edu>
2017-10-23 20:31:20 -0500

    ver3 checkpoint

--- clk_top.v
+++
@@ -1,8 +0,0 @@
-module clk_top(native_clk, f_clk_LED, s_clk_LED);
-	input native_clk;
-	output wire f_clk_LED, s_clk_LED;
-	
-	clk1kHz f_clk(native_clk, f_clk_LED);
-	clk1Hz  s_clk(native_clk, s_clk_LED);
-	
-endmodule
